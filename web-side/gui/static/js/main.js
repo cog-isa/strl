2cd604e9a60a28881fdd725a729b672c327656f0
Екатерина Иванова <katerina492021@mail.ru>
2016-12-20 15:46:09 +0300

    Новый гуй. Продолжение

---
+++ web-side/gui/static/js/main.js
@@ -0,0 +1,22 @@
+(function($){
+    $(window).on("load",function(){
+
+        $(".overflow-list").mCustomScrollbar({
+            theme: "dark-2",
+            mouseWheel:{
+                deltaFactor: 9
+            }
+        });
+
+    });
+
+
+    // Добавить свернуть/развернуть список для слоев
+
+
+
+
+
+
+
+})(jQuery);
