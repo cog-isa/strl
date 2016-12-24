eba66000f3e8958df6a7df9bbc3c9c191e0eddd2
Олег Большаков <bolshakov@nppsatek.ru>
2016-12-24 19:59:08 +0300

    Комит, чтобы дома продолжить

--- web-side/gui/static/js/listofproj.js
+++ web-side/gui/static/js/listofproj.js
@@ -1,77 +1,107 @@
 ﻿function listOfProjScope($scope) {
 	
-	console.log("Вошли в listOfProjScope");
+	console.log("Вошли в listOfProjScope");	
+
+	$scope.getAllProjects = getAllProjects;	
+	$scope.getProjectWorlds = getProjectWorlds;
+	$scope.createProject = createProject;
+	$scope.getProjectIndexById = getProjectIndexById;	
 	
-	// *********************************************
-	// Забиваем модель фейковыми данными
-	//
-	$scope.projects =
-	[
-	{id : 1, name : "Проект 1"},
-	{id : 2, name : "Проект 2"},
-	{id : 3, name : "Проект 3"}
-	];
+	initFakeStrucs(); // создать структуры даннных с фейковыми данными	
 	
-	// Индекс выделенного проекта
-	$scope.selectedPrjIndex = 0;
+	getAllProjects();
 	
-	// список миров по каждому проекту
-	$scope.projectsToWorlds =
-	[
-	{id : 1, world : {id : 1, name : "Мир 1", project_id : 1} },
-	{id : 2, world : {id : 2, name : "Мир 2", project_id : 2} },
-	{id : 3, world : {id : 3, name : "Мир 3", project_id : 3} }
-	];	
-	
-	//***************************************    
-	
-	
-	
-	/*
-	$scope.getProjectIndexById(proj_id)
-	{		
-		for (var i = 0; i < projects.length; i++) {
-			if (projects[i].id == proj_id)
+	// ---------------------------------
+	// Ниже идет определение всех функций
+		
+	function initFakeStrucs() // Забить модель фейковыми данными
+	{			
+		$scope.selectedPrjIndex = 0; // Индекс выделенного проекта в списке проектов
+				
+		$scope.projects = // список проектов
+		[
+		{id : 1, name : "Проект 1"},
+		{id : 2, name : "Проект 2"},
+		{id : 3, name : "Проект 3"}
+		];			
+
+		$scope.projectsToWorlds = // список миров по каждому проекту
+		[
+		{id : 1, world : {id : 1, name : "Мир 1", project_id : 1} },
+		{id : 2, world : {id : 2, name : "Мир 2", project_id : 2} },
+		{id : 3, world : {id : 3, name : "Мир 3", project_id : 3} }
+		];	
+	}
+			
+	function createProject() =  {		
+		$.ajax('/api/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            dataType: "json",
+			data: JSON.stringify({
+                        "name": "Динамически новый проект"
+                    })
+        }).fail(function () {
+			console.error("Ошибка получения списка проектов");
+        }).done(function (result) {			
+			var abc = result;
+            $scope.$scan();
+        });
+		
+		$scope.$scan();
+	}
+			
+	function getProjectIndexById(proj_id) {		
+		for (var i = 0; i < $scope.projects.length; i++) {
+			if ($scope.projects[i].id == proj_id)
 				return i;
 		}
 		
 		console.error("Не найден проект с id = " + proj_id);
 		
 		return -1;
-	}*/
-	
+	}	
 	
 	// получить список всех проектов
-	$.ajax('/api/projects', {
+	function getProjects() =  {
+		$.ajax('/api/projects', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8'
             },
-            dataType: "json"
+            dataType: "json"			
         }).fail(function () {
 			console.error("Ошибка получения списка проектов");
-        }).done(function (result) {
+        }).done(function (result) {	
+			//var oldSelectedPrjId = $scope.projects[$scope.selectedPrjIndex].id;
+			$scope.projects = result;
 			$scope.selectedPrjIndex = 0;
-            $scope.projects = result;
-			if ($scope.projects.length)
-            $scope.$scan();
-        });
+			$scope.getProjectWorlds($scope.projects[$scope.selectedPrjIndex].id);
+			$scope.$scan();
+        });		
 		
 	// получить список всех миров заданного проекта
-	$.ajax('/api/projects/' + $scope.projects[$scope.selectedPrjIndex].id + '/worlds', {
+	function getProjectWorlds (proj_id) {		
+		$.ajax('/api/projects/' + proj_id + '/worlds', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json; charset=UTF-8'
             },
             dataType: "json"
         }).fail(function () {
-			console.error("Ошибка получения списка миров проекта " + $scope.projects[$scope.selectedPrjIndex]);
-        }).done(function (result) {
-            $scope.projectsToWorlds[$scope.selectedPrjIndex] = result;
-            $scope.$scan();
-        });
-	
-	
+			console.error("Ошибка получения списка миров проекта " + proj_id);
+        }).done(function (result) {			
+			$scope.projectsToWorlds[getProjectIndexById(proj_id)] = worlds;
+			$scope.$scan();
+            updateProjectWorlds(, result)
+        });	
+
+	// установить список миров в проект
+	function updateProjectWorlds(proj_index, worlds){
+		
+	}
 
     window.S = $scope;
 }
