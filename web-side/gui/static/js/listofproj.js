07afc1bf8a6fbc64723b95f3a0dec7c7d847a546
Олег Большаков <bolshakov@nppsatek.ru>
2016-12-26 15:26:46 +0300

    Работает создание проектом и миров и меняется название на константное при переименовании

--- web-side/gui/static/js/listofproj.js
+++ web-side/gui/static/js/listofproj.js
@@ -6,8 +6,7 @@
 	// ---------------------------------
 	// Ниже идет определение всех функций
 	
-	function initScope() 
-	{
+	function initScope() {
 		$scope.getProjects = getProjects;
 		$scope.createProject = createProject;
 		$scope.editProject = editProject;
@@ -19,37 +18,39 @@
 		$scope.selectProject = selectProject;
 		$scope.selectWorld = selectWorld;
 		
-		$scope.selectedPrjIndex = 0; // Индекс выделенного проекта в списке проектов
-		$scope.selectedWorldIndex = 0; // Индекс выделенного мире в списке проектов
+		$scope.selectedPrj; // Индекс выделенного проекта в списке проектов
+		$scope.selectedWorld; // Индекс выделенного мире в списке проектов
 				
 		$scope.projects = [		// список проектов 
 			{id : 1, name : "Проект 1"},
 			{id : 2, name : "Проект 2"},
 			{id : 3, name : "Проект 3"}
-		];			
+		];
 
-		$scope.projectsToWorlds = [		// список миров по каждому проекту				
-			[ {id : 1, name : "Мир 1_1", project_id : 1} ], // массив миров 1-го проекта
-			[ {id : 2, name : "Мир 2_1", project_id : 2} ], // массив миров 2-го проекта 
-			[ {id : 3, name : "Мир 3_1", project_id : 3} ]	// массив миров 3-го проекта 	
+		$scope.worlds = [ // список миров		
+			{id : 1, name : "Мир 1_1", project_id : 1}, 
+			{id : 2, name : "Мир 2_1", project_id : 2}, 
+			{id : 3, name : "Мир 3_1", project_id : 3}
 		];	
 	}				
 		
-	function getProjects() =  {
+	function getProjects() {
 		$.ajax('/api/projects', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json; charset=UTF-8'},
             dataType: "json"			
         }).fail(function () {
 			console.error("Ошибка получения списка проектов");
-        }).done(function (result) {	
-			//var oldSelectedPrjId = $scope.projects[$scope.selectedPrjIndex].id;
+			$scope.$scan();
+        }).done(function (result) {		
 			$scope.projects = result;
-			$scope.selectedPrjIndex = 0;			
-			getWorlds($scope.selectedPrjIndex);			
-        });	
+			$scope.selectedPrj = null;
+			$scope.worlds = [];			
+			$scope.$scan();
+        });			
+	}
 			
-	function createProject() =  {		
+	function createProject() {		
 		$.ajax('/api/projects', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json; charset=UTF-8'},
@@ -59,103 +60,109 @@
                     })
         }).fail(function () {
 			console.error("Ошибка создания проекта");
+			getProjects();
         }).done(function (result) {			
-			getProjects();            
-        });		
+			getProjects();
+        });					
 	}
 	
-	function editProject(projIndex, newName) =  {		 
-		$.ajax('/api/projects/' + $scope.projects[projIndex], {
+	function editProject(proj, newName) {		 
+		$.ajax('/api/projects/' + proj.id, {
             method: 'PATCH',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8'},
-            dataType: "json",
+            headers: { 'Content-Type': 'application/json; charset=UTF-8'},            
 			data: JSON.stringify({
                         "name": newName
                     })
         }).fail(function () {
 			console.error("Ошибка редактирования проекта");
+			getProjects();
         }).done(function (result) {			
-			getProjects();            
-        });		
+			getProjects();
+        });	
 	}
 	
-	function deleteProject(projIndex) =  {		 
-		$.ajax('/api/projects/' + $scope.projects[projIndex], {
+	function deleteProject(proj) {		 
+		$.ajax('/api/projects/' + proj.id, {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json; charset=UTF-8' },
             dataType: "json"			
         }).fail(function () {
 			console.error("Ошибка удаления проекта");
+			getProjects();
         }).done(function (result) {			
-			getProjects();            
-        });		
+			getProjects();
+        });			
 	}	
 		
-	function getWorlds(proj_index) {		
+	function getWorlds(proj_id) {		
 		$.ajax('/api/projects/' + proj_id + '/worlds', {
             method: 'GET',
             headers: { 'Content-Type': 'application/json; charset=UTF-8' },
             dataType: "json"
         }).fail(function () {
 			console.error("Ошибка получения списка миров проекта");
+			$scope.$scan();
         }).done(function (result) {			
-			$scope.projectsToWorlds[proj_index] = worlds;
-			$scope.$scan();            
-        });	
+			$scope.worlds = result;	
+			$scope.$scan();
+        });				
+	}
 		
-	function createWorld(proj_index) =  {		
+	function createWorld(proj) {		
 		$.ajax('/api/worlds', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json; charset=UTF-8'},
             dataType: "json",
 			data: JSON.stringify({
 				"name": "Новый мир",
-				"project_id": projects[proj_index].id
+				"project_id": proj.id
 			})
         }).fail(function () {
 			console.error("Ошибка создания мира");
+			getWorlds(proj.id);
         }).done(function (result) {			
-			getWorlds(proj_index);
-        });		
+			getWorlds(proj.id);
+        });			
 	}
 	
-	function editWorld(proj_index, world_index, newName) =  {		
-		$.ajax('/api/worlds/' + $scope.projectsToWorlds[proj_index][world_index].id, {
+	function editWorld(world, newName) {		
+		$.ajax('/api/worlds/' + world.id, {
             method: 'PATCH',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8'},
-            dataType: "json",
+            headers: { 'Content-Type': 'application/json; charset=UTF-8'},            
 			data: JSON.stringify({
 				"name": newName,
 			})
         }).fail(function () {
 			console.error("Ошибка редактирования мира");
+			getWorlds(world.project_id);
         }).done(function (result) {			
-			getWorlds(proj_index);
-        });		
+			getWorlds(world.project_id);
+        });			
 	}
 	
-	function deleteWorld(projIndex, worldIndex) =  {		 
-		$.ajax('/api/worlds/' + $scope.projectsToWorlds[projIndex][worldIndex].id, {
+	function deleteWorld(world) {		 
+		$.ajax('/api/worlds/' + world.id, {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json; charset=UTF-8' },
             dataType: "json",
 			data: ""
         }).fail(function () {
 			console.error("Ошибка удаления мира");
+			getWorlds(world.project_id);
         }).done(function (result) {			
-			getWorlds(projIndex);
-        });		
+			getWorlds(world.project_id);
+        });			
 	}	
 	
-	function selectProject(index) = {
-		$scope.selectedPrjIndex = index;
-		$scope.selectedWorldIndex = 0;
-		getWorlds(index);
+	function selectProject(proj) {
+		$scope.selectedPrj = proj;
+		$scope.selectedWorld = null;
+		getWorlds(proj.id);
 	}
 	
-	function selectWorld(index) = {		
-		$scope.selectedWorldIndex = index;
-		$scope.scan();
+	function selectWorld(world) {		
+		$scope.selectedWorld = world;
+		$scope.$scan();
 	}
 	
 	/*
